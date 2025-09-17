2ea41e1c4b0b4551053331ed7f5c2eac6d00d778
Hozaifa <hozaifabinmasud@gmail.com>
2025-09-17 21:26:02 +0600

    Create guards

---
+++ backend/src/auth/guards/role.guard.ts
@@ -0,0 +1,29 @@
+import { CanActivate, ExecutionContext, Injectable } from '@nestjs/common';
+import { Reflector } from '@nestjs/core';
+import { ROLE_KEY } from '../decorators/roles.decorator';
+import { Request } from 'express';
+import { AuthUserType, RoleEnum } from '@/types/auth';
+import { Role } from '@/db/schemas';
+
+@Injectable()
+export class RolesGuard implements CanActivate {
+   constructor(private readonly reflector: Reflector) {}
+
+   canActivate(context: ExecutionContext): boolean | Promise<boolean> {
+      const requiredRoles = this.reflector.getAllAndOverride<
+         Role[] | undefined
+      >(ROLE_KEY, [context.getHandler(), context.getClass()]);
+
+      if (!requiredRoles) {
+         return true;
+      }
+
+      const request = context
+         .switchToHttp()
+         .getRequest<Request & { user: AuthUserType }>();
+      const userRole = request.user.role;
+      const rolesSet = new Set([...requiredRoles, RoleEnum.super_admin]);
+
+      return rolesSet.has(userRole);
+   }
+}
